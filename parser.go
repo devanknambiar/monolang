9949920bdcf77d7de508f9f1f0bca975e595b1cc
devanknambiar <dnambi05@gmail.com>
2026-06-28 20:38:34 +0530

    dummy parseStatement method

--- parser.go
+++ parser.go
@@ -19,3 +19,25 @@ func NewParser(l *Lexer) *Parser {
 	p.nextToken() //for p.nextToken
 	return p
 } 
+
+//parseStatement() method - Statement
+func (p *Parser) parseStatement() Statement {
+	switch p.curToken.Type {
+	case LET:
+		return p.parseLetStatement()
+	
+	case RETURN:
+		return p.parseReturnStatement()
+	
+	default:
+		return nil
+	}
+}
+
+func (p *Parser) parseLetStatement() *LetStatement {
+	return nil
+}
+
+func (p *Parser) parseReturnStatement() *LetStatement {
+	return nil
+}
